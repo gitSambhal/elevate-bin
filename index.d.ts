763d621affecb095915e20ef48253019fcfd2ea9
Suhail Akhtar <sambhal@outlook.com>
2023-12-08 17:16:07 +0530

    Exported variables as paths to binaries

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export const pathElevate: string;
+export const pathElevate64: string;
---
+++ index.js
@@ -0,0 +1,4 @@
+const path = require("path");
+
+exports.pathElevate = path.join(__dirname, "bin", "elevate.exe")
+exports.pathElevate64 = path.join(__dirname, "bin", "elevate-x64.exe")
